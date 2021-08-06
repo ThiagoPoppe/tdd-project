458ef1fabe2761358247ccc1878dd81d27b25f6d
ThiagoPoppe <thiagopoppe@dcc.ufmg.br>
2021-08-05 21:46:37 -0300

    Não Salvar Items em Branco a cada Requisição

--- lists/tests.py
+++ lists/tests.py
@@ -7,6 +7,10 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
@@ -16,7 +20,7 @@ class HomePageTest(TestCase):
 
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
-        
+
 
 class ItemModelTest(TestCase):
 
